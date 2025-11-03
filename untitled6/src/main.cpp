8ec51c0182804629585e84fe4ddb1c86d0b5cb39
Arianna <ardilesarianna@gmail.com>
2025-11-03 11:23:34 -0500

    Added functionalities to hashtable and added college placement dataset (Issue #5)

--- untitled6/src/main.cpp
+++ untitled6/src/main.cpp
@@ -5,7 +5,12 @@ using namespace std;
 int main() {
     ifstream file("data/graduates.csv");
     if (!file.is_open()) {
-        cout << "Error opening file" << endl;
+        cout << "Error file opening file" << endl;
+        return 1;
+    }
+    ifstream file1("data/college_student_placement_dataset.csv");
+    if (!file1.is_open()) {
+        cout << "Error file1 opening file" << endl;
         return 1;
     }
     return 0;
