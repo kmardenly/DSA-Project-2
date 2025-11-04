7222d03217b44f9cf82b3ff971a7b67d6fad44ee
ajohnson21-uf <ajohnson21@ufl.edu>
2025-11-04 12:14:01 -0500

    Update main.cpp

--- untitled6/src/main.cpp
+++ untitled6/src/main.cpp
@@ -1,17 +1,74 @@
 #include <fstream>
 #include <iostream>
+#include "Menu.h"
+//#include "hash.h"
+#include "red-black.h"
 using namespace std;
 
-int main() {
-    ifstream file("data/graduates.csv");
-    if (!file.is_open()) {
-        cout << "Error file opening file" << endl;
-        return 1;
+
+
+
+
+int main(){
+    //setup the loop to stay on the menu unless exited, starts off with no data loaded and no data structure chosen, terminal input, initializing tree, Hashtable has an error currently so commented out
+    bool exited = false;
+    bool dsrb = false;
+    bool loaded = false;
+    int new_input = 0;
+    RedBlack rbtree;
+   // HashTable<string, int> hashtree(10000);
+
+    while(!exited) {
+        DisplayMenu(dsrb, loaded);
+        string input;
+        cin >> input;
+        try{new_input = stoi(input);}
+        catch(...){ cout << "Invalid Input" << endl; continue;}
+        switch(new_input) {
+        // different menu options are in Menu.cpp( AJ will keep working on different options)
+            case 0:
+                exited = true;
+                break;
+            case 1:
+               LoadDataSet(rbtree, "data/college_student_placement_dataset.csv"); // hashtree,
+                dsrb = true;
+                loaded = true;
+                cout;
+                break;
+            case 2:
+                cout;
+                break;
+            case 3:
+                cout;
+                break;
+            case 4:
+                cout;
+                break;
+            case 5:
+                cout;
+                break;
+            case 6:
+                cout;
+                break;
+            case 7:
+                cout;
+                break;
+            case 8:
+                cout;
+                break;
+            case 9:
+                cout;
+                break;
+            case 10:
+                cout;
+                break;
+            case 11:
+                cout;
+                break;
+            case 12:
+                dsrb = false;
+                break;
+        }
     }
-    ifstream file1("data/college_student_placement_dataset.csv");
-    if (!file1.is_open()) {
-        cout << "Error file1 opening file" << endl;
-        return 1;
-    }
-    return 0;
+
 }
